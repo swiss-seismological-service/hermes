33a79651a2730eb3a68e22107f93d2bbb568c26c
Lukas Heiniger <lukas.heiniger@sed.ethz.ch>
2013-12-09 16:10:14 +0100

    Add simple passive scheduler to run tasks repeatedly

---
+++ atlas/model/taskscheduler.py
@@ -0,0 +1,154 @@
+# -*- encoding: utf-8 -*-
+"""
+Provide classes to run a simple task scheduler
+    
+Copyright (C) 2013, ETH Zurich - Swiss Seismological Service SED
+
+"""
+
+import logging
+import inspect
+
+class ScheduledTask:
+    """
+    A scheduled task.
+
+    The task manages its own scheduling interval and time for next execution.
+
+
+    """
+    def __init__(self, task_function, dt=None, name='Task'):
+        """
+        Create a new task by providing a function to run at the
+        scheduled time and an optional scheduling interval for repeating tasks.
+        The name is optional too and is simply used for logging purposes.
+
+        :param task_function: the function which TaskScheduler invokes when the
+            task should run. This function must take one argument which is the
+            run_info object that contains everything the task needs in order
+            to execute.
+        :param dt: optional scheduling interval for repeating tasks
+        :type dt: timedelta
+        :param name: name of the task
+        :type name: str
+
+        """
+        self.name = name
+        self.dt = dt
+        self.run_time = None
+        self.task_function = task_function
+
+    def is_pending(self, t):
+        """
+        Return True if execution is pending at time *t*
+
+        :param t: time to check against scheduled execution time
+        :type t: datetime
+
+        """
+        return t >= self.run_time if self.run_time is not None else False
+
+    def schedule(self, t_run=None):
+        """
+        Schedule the next execution at t_run.
+
+        For a repeating task, t_run does not have to provided (except on the
+        first call). In that case the tasks next execution will be scheduled on
+        the previous execution time + dt.
+
+        :param t_run: time at which to run the task
+        :type t_run: datetime
+
+        """
+        if t_run is None:
+            self.run_time += self.dt
+        else:
+            self.run_time = t_run
+
+    def run(self, run_info):
+        """
+        Runs the task by invoking the run_function and passing the run_info
+        dictionary to it.
+
+        """
+        self.task_function(run_info)
+
+
+class TaskScheduler:
+    """
+    Manages and executes scheduled tasks.
+
+    You add tasks to the list using add_task.
+
+    """
+
+    def __init__(self):
+        """
+        Initialize a scheduler with a list of tasks.
+
+        :param task_list: list of *ScheduledCoreTask* tasks
+
+        """
+        # Maintain a list of scheduled tasks
+        self.scheduled_tasks = []
+        self._logger = logging.getLogger(__name__)
+
+    def add_task(self, task):
+        """
+        Add a new task to the scheduler
+
+        :param task: Task to add
+        :type task: ScheduledTask
+
+        """
+        self.scheduled_tasks.append(task)
+
+    def reset_schedule(self, t0):
+        """
+        Reset the scheduled times by scheduling the first runs for all repeating
+        tasks based on t0. Non-repeating tasks are removed.
+
+        """
+        self.scheduled_tasks = [task for task in self.scheduled_tasks
+                                if task.dt is not None]
+        for task in self.scheduled_tasks:
+            task.run_time = t0 + task.dt
+
+    def pending_tasks(self, t):
+        return [task for task in self.scheduled_tasks if task.is_pending(t)]
+
+    def has_pending_tasks(self, t):
+        return True if len(self.pending_tasks(t)) > 0 else False
+
+    def run_pending_tasks(self, t, run_info):
+        """
+        Run all tasks that are pending at time t. After running, repeating tasks
+        will are scheduled for the next execution and non-repeating tasks are
+        removed from the task queue.
+
+        :param t: time t against which the schedule is checked
+        :type t: datetime
+        :param run_info: object that will be passed to the task function
+
+        """
+        for task in self.scheduled_tasks:
+            if task.is_pending(t):
+                self._logger.info('Running Task: ' + task.name)
+                task.run(run_info)
+
+        self._update_schedule(t)
+
+    def _update_schedule(self, t):
+        """
+        Schedule next task execution.
+
+        The function checks which tasks need rescheduling at time t and
+        schedules their next execution.
+
+        """
+        for task in self.scheduled_tasks:
+            if task.is_pending(t):
+                if task.dt is None:
+                    self.scheduled_tasks.remove(task)
+                else:
+                    task.schedule()
