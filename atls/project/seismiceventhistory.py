c13c806393e08f7a1ffcd78dd945cc8afd31ee18
Lukas Heiniger <lukas.heiniger@sed.ethz.ch>
2014-06-02 10:43:48 +0200

    Make importing more robust to errors

--- atls/project/seismiceventhistory.py
+++ atls/project/seismiceventhistory.py
@@ -8,6 +8,8 @@ from eventhistory import EventHistory
 from eventimporter import EventImporter
 from domainmodel.seismicevent import SeismicEvent
 from domainmodel.location import Location
+import logging
+import traceback
 
 
 class SeismicEventHistory(EventHistory):
@@ -19,27 +21,38 @@ class SeismicEventHistory(EventHistory):
 
     def __init__(self, store):
         super(SeismicEventHistory, self).__init__(store, SeismicEvent)
+        self._logger = logging.getLogger(__name__)
 
     def import_events(self, importer):
         """
-        Imports seismic events from a csv file by using an EventReporter
+        Imports seismic events from a csv file by using an EventImporter
 
         The EventReporter must return the following fields (which must thus
         be present in the csv file)
             lat: latitude
             lon: longitude
+            depth: depth [m], positive downwards
             mag: magnitude
         :param importer: an EventImporter object
         :type importer: EventImporter
 
         """
-        self.store.purge(self.entity)
-
         events = []
-        for date, fields in importer:
-            location = Location(float(fields['lon']), float(fields['lat']))
-            event = SeismicEvent(date, float(fields['mag']), location)
-            events.append(event)
-        self.store.add(events)
-        self.reload_from_store()
-        self._emit_change_signal({})
+        try:
+            for date, fields in importer:
+                location = Location(float(fields['lon']), float(fields['lat']),
+                                    -float(fields['depth']))
+                event = SeismicEvent(date, float(fields['mag']), location)
+                events.append(event)
+        except:
+            self._logger.error('Failed to import events. Make sure the .csv '
+                               'file contains lon, lat, depth, and mag fields '
+                               'and that the date field has the format '
+                               'dd.mm.yyyyTHH:MM:SS. The original error was '
+                               + traceback.format_exc())
+        else:
+            self.store.purge(self.entity)
+            self.store.add(events)
+            self._logger.info('Imported {} events.'.format(len(events)))
+            self.reload_from_store()
+            self._emit_change_signal({})
