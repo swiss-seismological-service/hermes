7df36320d97513bafb628edcf64c31a04bf0d635
Lukas Heiniger <lukas.heiniger@sed.ethz.ch>
2015-10-07 10:20:41 +0200

    Add missing sphinx theme to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ wget --quiet -O - $PUBLIC_KEY | sudo apt-key add -
 DEB_PACKAGES="python-qt4 python-qt4-gl qgis python-mock python-obspy"\
 " python-sqlalchemy python-pip python-oq-engine python-nose python-lxml git"\
 " graphviz"
-PIP_PACKAGES="numpy pymatlab sphinx"
+PIP_PACKAGES="numpy pymatlab sphinx sphinx-rtd-theme"
 
 # install deb and pip packages
 apt-get update
