2bb491ad1261053976e4ac9815bdd10a1b0c6976
Alexander Walsh <alexander.walsh@sed.ethz.ch>
2015-12-01 11:34:24 +0100

    Rename detachedrunners to runners

--- ramsis/core/ismodelcontrol.py
+++ ramsis/core/ismodelcontrol.py
@@ -9,7 +9,7 @@ I.e. this is also the place where you add new models to the system.
 
 import logging
 
-from detachedrunners import ModelRunner
+from runners import ModelRunner
 from ismodels.rj import Rj
 from ismodels.etas import Etas
 
