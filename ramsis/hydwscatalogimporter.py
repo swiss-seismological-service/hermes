f3bcff4ad5e647b16798191e9f3f417bbd69e87e
Alexander Walsh <alexander.walsh@sed.ethz.ch>
2016-01-05 10:06:55 +0100

    Use correct dictionary access syntax and convert date to datetime object in HYDWSCatalogImporter.

--- ramsis/hydwscatalogimporter.py
+++ ramsis/hydwscatalogimporter.py
@@ -1,14 +1,18 @@
+from datetime import datetime
+
+
 class HYDWSCatalogImporter:
     def __init__(self, catalog):
         self.catalog = catalog
 
     def __iter__(self):
         for event in self.catalog:
-            date = event.time
+            date = event['time']['value']
+            date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S')
             row = {
-                'flow_dh': event.bottomHoleFlowRate,
-                'flow_xt': event.topHoleFlowRate,
-                'pr_dh': event.bottomHolePressure,
-                'pr_xt': event.topHolePressure
+                'flow_dh': event['bottomHoleFlowRate']['value'],
+                'flow_xt': event['topHoleFlowRate']['value'],
+                'pr_dh': event['bottomHolePressure']['value'],
+                'pr_xt': event['topHolePressure']['value']
             }
             yield (date, row)
