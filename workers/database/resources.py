7f48f968f26432548080c7bbfc1f301c5bb601a4
Alexander Walsh <alexander.walsh@sed.ethz.ch>
2016-03-18 16:59:51 +0100

    Persist model output to database

--- workers/database/resources.py
+++ workers/database/resources.py
@@ -3,15 +3,44 @@ import json
 from flask import request
 from flask_restful import Resource
 
+from data.modelresultslayer import ModelResultsLayer
+from data.modelresultsitem import ModelResultsItem
 from model.common import ModelOutput
+from data.store import Store
+from data.ormbase import OrmBase
 
 
 class Rj(Resource):
+    store = Store("sqlite:///db.sqlite", OrmBase)
+
     def post(self, job_id):
         data = json.loads(request.form["data"])
 
         model_output = ModelOutput(None, None, None)
         model_output.deserialize(data["model_output"])
+
+        item = self._get_model_results_item(model_output)
+        layer = ModelResultsLayer(self.store, ModelResultsItem)
+        layer.add(item, persist=True)
+
+    @staticmethod
+    def _get_model_results_item(model_output):
+        r = model_output.cum_result
+        rate = r.rate if r else None
+        b_val = r.b_val if r else None
+        prob = r.prob if r else None
+
+        item = ModelResultsItem(
+            model_output.failed,
+            model_output.failure_reason,
+            model_output.t_run,
+            model_output.dt,
+            rate,
+            b_val,
+            prob
+        )
+
+        return item
 
 
 class RjId(Resource):
