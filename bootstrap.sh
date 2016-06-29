0738701df1967678ed606ac006caaf7af479f346
Alexander Walsh <alexander.walsh@sed.ethz.ch>
2016-06-29 14:32:01 +0200

    Add sphinx and epydoc dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,8 +18,10 @@ wget --quiet -O - $PUBLIC_KEY | sudo apt-key add -
 
 # dependencies for RAMSIS
 DEB_PACKAGES=""\
+" dvipng=1.14-2"\
 " git=1:1.9.1-1ubuntu0.3"\
 " graphviz=2.36.0-0ubuntu3.1"\
+" python-epydoc=3.0.1+dfsg-4"\
 " python-lxml=3.3.3-1ubuntu0.1"\
 " python-mock=1.0.1-3"\
 " python-nose=1.3.1-2"\
@@ -29,7 +31,8 @@ DEB_PACKAGES=""\
 " python-qt4=4.10.4+dfsg-1ubuntu1"\
 " python-qt4-gl=4.10.4+dfsg-1ubuntu1"\
 " python-sqlalchemy=0.8.4-1build1"\
-" qgis=2.0.1-2build2"
+" qgis=2.0.1-2build2"\
+" texlive-latex-base=2013.20140215-1"
 PIP_PACKAGES=""\
 " numpy==1.8.2"\
 " pymatlab==0.2.3"\
