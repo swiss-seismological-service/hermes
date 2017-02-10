3ac5d7c52b01dc1321a36efadbd8f4ebdfbc9a39
Lukas Heiniger <lukas.heiniger@sed.ethz.ch>
2017-02-10 11:37:53 +0100

    Generate more dummy data for testing

--- ramsis/ramsis/ui/forecastwindow/data.py
+++ ramsis/ramsis/ui/forecastwindow/data.py
@@ -9,7 +9,7 @@ Copyright (C) 2015, SED (ETH Zurich)
 """
 
 from PyQt4.QtCore import pyqtSignal, QObject
-from datetime import datetime
+from datetime import datetime, timedelta
 from ramsisdata.forecast import ForecastSet, Forecast, ForecastInput,\
     ForecastResult, Scenario, SkillTest, ModelResult, RatePrediction
 
@@ -25,24 +25,26 @@ class DummyProject(QObject):
 def dummy_data():
     fc_set = ForecastSet()
 
-    forecast = Forecast()
-    forecast.forecast_time = datetime(2016, 11, 11, 12, 00, 00)
-    forecast_result = ForecastResult()
-    forecast.result.append(forecast_result)
-    model_result = ModelResult()
-    model_result.model_name = 'rj'
-    forecast.result[0].model_results[model_result.model_name] = model_result
-    model_result = ModelResult()
-    model_result.model_name = 'etas'
-    forecast.result[0].model_results[model_result.model_name] = model_result
-    forecast.input = ForecastInput()
-    scenario = Scenario()
-    scenario.name = 'Scenario 1'
-    forecast.input.scenarios.append(scenario)
-    scenario = Scenario()
-    scenario.name = 'Scenario 2'
-    forecast.input.scenarios.append(scenario)
-    fc_set.forecasts.append(forecast)
+    start = datetime(2016, 1, 1, 06, 00, 00)
+    for i in range(5):
+        forecast = Forecast()
+        forecast.forecast_time = start + timedelta(hours=i*6)
+        forecast_result = ForecastResult()
+        forecast.result.append(forecast_result)
+        model_result = ModelResult()
+        model_result.model_name = 'rj'
+        forecast.result[0].model_results[model_result.model_name] = model_result
+        model_result = ModelResult()
+        model_result.model_name = 'etas'
+        forecast.result[0].model_results[model_result.model_name] = model_result
+        forecast.input = ForecastInput()
+        scenario = Scenario()
+        scenario.name = 'Scenario 1'
+        forecast.input.scenarios.append(scenario)
+        scenario = Scenario()
+        scenario.name = 'Scenario 2'
+        forecast.input.scenarios.append(scenario)
+        fc_set.forecasts.append(forecast)
 
     return DummyProject(fc_set)
 
