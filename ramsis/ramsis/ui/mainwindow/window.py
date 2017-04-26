c226ad318d4b1dd84de292a777431f722121584e
Lukas Heiniger <lukas.heiniger@sed.ethz.ch>
2017-04-26 10:10:21 +0200

    Delete input catalogs when deleting forecast results

--- ramsis/ramsis/ui/mainwindow/window.py
+++ ramsis/ramsis/ui/mainwindow/window.py
@@ -254,12 +254,13 @@ class MainWindow(QtGui.QMainWindow):
         )
         if reply == QMessageBox.Yes:
             project = self.ramsis_core.project
-            self.logger.info('Deleting results for {} forecasts'
+            self.logger.info('Deleting results and input catalogs for {} '
+                             'forecasts'
                              .format(len(project.forecast_set.forecasts)))
             for forecast in project.forecast_set.forecasts:
                 forecast.results = []
+                forecast.input.input_catalog = None
             project.save()
-
 
     def _import_file_to_history(self, path, history, delimiter=' '):
         """
