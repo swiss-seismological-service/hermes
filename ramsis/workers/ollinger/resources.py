aeddb41edba442742c0ba7885e4258e5ff7e9663
Alexander Walsh <alexander.walsh@sed.ethz.ch>
2017-05-11 14:59:01 +0200

    Use single quotes in ollinger/resources.py

--- ramsis/workers/ollinger/resources.py
+++ ramsis/workers/ollinger/resources.py
@@ -9,25 +9,25 @@ from flask_restful import Resource
 
 
 class Run(Resource):
-    model_path = "C:\RAMSIS\Worker\Simulation_Test"
-    model_filename = "start_simulation.bat"
+    model_path = 'C:\RAMSIS\Worker\Simulation_Test'
+    model_filename = 'start_simulation.bat'
     seismic_catalog_filenames = [
-        "SeismicCatalog_00000.csv",
-        "SeismicCatalog_00001.csv",
-        "SeismicCatalog_00002.csv",
-        "SeismicCatalog_00003.csv",
-        "SeismicCatalog_00004.csv",
-        "SeismicCatalog_00005.csv",
-        "SeismicCatalog_00006.csv",
-        "SeismicCatalog_00007.csv",
-        "SeismicCatalog_00008.csv",
-        "SeismicCatalog_00009.csv"
+        'SeismicCatalog_00000.csv',
+        'SeismicCatalog_00001.csv',
+        'SeismicCatalog_00002.csv',
+        'SeismicCatalog_00003.csv',
+        'SeismicCatalog_00004.csv',
+        'SeismicCatalog_00005.csv',
+        'SeismicCatalog_00006.csv',
+        'SeismicCatalog_00007.csv',
+        'SeismicCatalog_00008.csv',
+        'SeismicCatalog_00009.csv'
     ]
 
     def post(self):
         try:
             print('Model run started')
-            data = json.loads(request.form["data"])
+            data = json.loads(request.form['data'])
             print('Writing seismic catalog...')
             self._write_seismic_catalog(data)
             print('Running...')
@@ -47,11 +47,11 @@ class Run(Resource):
                 with open(os.path.join(self.model_path, filename)) as f:
                     pass  # TODO: calculate statistics
             model_result = json.dumps({
-                "skill_test": None,
-                "failed": False,
-                "model_name": "",
-                "rate_prediction": None,
-                "failure_reason": ""
+                'skill_test': None,
+                'failed': False,
+                'model_name': '',
+                'rate_prediction': None,
+                'failure_reason': ''
             })
             return model_result
         except:
@@ -61,17 +61,17 @@ class Run(Resource):
         with open('Simulation_Test\SeismicCatalog_Measured.csv', 'wb')\
                 as csvfile:
             writer = csv.writer(csvfile, delimiter=';')
-            writer.writerow(["Time", "Offset-X(m)", "Offset-Y(m)",
-                             "Offset-Z(m)", "Local magnitude"])
-            events = data["forecast"]["input"]["input_catalog"]
-            events = events["seismic_events"]
+            writer.writerow(['Time', 'Offset-X(m)', 'Offset-Y(m)',
+                             'Offset-Z(m)', 'Local magnitude'])
+            events = data['forecast']['input']['input_catalog']
+            events = events['seismic_events']
             try:
                 for e in events:
-                    d = datetime.strptime(e["date_time"],
+                    d = datetime.strptime(e['date_time'],
                                           '%Y-%m-%dT%H:%M:%S+00:00')
-                    e["date_time"] = d.strftime('%d.%m.%Y %H:%M:%S.0000')
-                    row = [e[key] for key in ["date_time", "x", "y", "z",
-                                              "magnitude"]]
+                    e['date_time'] = d.strftime('%d.%m.%Y %H:%M:%S.0000')
+                    row = [e[key] for key in ['date_time', 'x', 'y', 'z',
+                                              'magnitude']]
                     writer.writerow(row)
             except TypeError:
                 print('Could not write catalog: no seismic events')
