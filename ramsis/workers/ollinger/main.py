0c946b524d0911875b4e85c846ce32f92cee5610
Lukas Heiniger <lukas.heiniger@sed.ethz.ch>
2017-05-12 14:56:03 +0200

    Enable proper logging on ollinger worker

--- ramsis/workers/ollinger/main.py
+++ ramsis/workers/ollinger/main.py
@@ -1,15 +1,25 @@
-from flask import Flask
+import logging
+from flask import Flask, got_request_exception
 from flask_restful import Api
 
 from resources import Run
 
 
+def log_exception(sender, exception, **extra):
+    sender.logger.debug('Got exception during processing: %s', exception)
+
+
 def main():
-    app = Flask(__name__)
+    app = Flask('ollinger_worker')
+    app.debug = True
     api = Api(app)
     api.add_resource(Run, '/run')
 
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.logger.setLevel(logging.DEBUG)
+    got_request_exception.connect(log_exception, app)
+    app.logger.info('Starting Ollinger Worker')
+
+    app.run(host='0.0.0.0', port=8080, use_reloader=False)
 
 
 if __name__ == '__main__':
