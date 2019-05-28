62d94581f8ea8041ae3169393adf6d935e3c173a
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-05-28 14:48:28 +0000

    [TST]: Provide unittest facilities
    Implement rudimentary unittests for injectionwell serialization (both
    hydraulics (past) and injectionplan (future scenario)).

--- RAMSIS/io/tests/test_hydraulics.py
+++ RAMSIS/io/tests/test_hydraulics.py
@@ -15,13 +15,16 @@ from ramsis.datamodel.status import Status  # noqa
 from ramsis.datamodel.seismicity import SeismicityModel  # noqa
 from ramsis.datamodel.forecast import Forecast  # noqa
 from ramsis.datamodel.seismics import SeismicCatalog, SeismicEvent  # noqa
-from ramsis.datamodel.well import InjectionWell  # noqa
-from ramsis.datamodel.hydraulics import Hydraulics, InjectionPlan  # noqa
+from ramsis.datamodel.well import InjectionWell, WellSection  # noqa
+from ramsis.datamodel.hydraulics import (Hydraulics, InjectionPlan,  # noqa
+                                         HydraulicSample) # noqa
 from ramsis.datamodel.settings import ProjectSettings  # noqa
 from ramsis.datamodel.project import Project  # noqa
 
-from RAMSIS.io.hydraulics import HYDWSBoreholeHydraulicsDeserializer
-from RAMSIS.io.utils import binary_request, pymap3d_transform_geodetic2ned
+from RAMSIS.io.hydraulics import (HYDWSBoreholeHydraulicsDeserializer,
+                                  HYDWSBoreholeHydraulicsSerializer)
+from RAMSIS.io.utils import (binary_request, pymap3d_transform_geodetic2ned,
+                             pymap3d_transform_ned2geodetic)
 
 
 class HYDWSBoreholeHydraulicsDeserializerTestCase(unittest.TestCase):
@@ -129,8 +132,132 @@ class HYDWSBoreholeHydraulicsDeserializerTestCase(unittest.TestCase):
                          datetime.datetime(2019, 5, 3, 15, 27, 9, 117623))
 
 
+class HYDWSBoreholeHydraulicsSerializerTestCase(unittest.TestCase):
+    """
+    Test for the
+    :py:class:`RAMSIS.io.hydraulics.HYDWSBoreholeHydraulicsSerializer` class.
+    """
+
+    def test_with_hydraulics(self):
+        # XXX(damb): Depending on the coordinate transformation used the
+        # results might vary.
+        reference_result = {
+            'sections': [{
+                'toplongitude': {'value': 10.663207130000002},
+                'toplatitude': {'value': 10.66320713},
+                'topdepth': {'value': 0.0},
+                'bottomlatitude': {'value': 10.66320713},
+                'bottomlongitude': {'value': 10.66320713},
+                'bottomdepth': {'value': 1000.0000000004027},
+                'holediameter': {'value': 0.3},
+                'topclosed': False,
+                'bottomclosed': False,
+                'publicid': ('smi:ch.ethz.sed/bh/section/'
+                             '11111111-8d89-4f13-95e7-526ade73cc8b'),
+                'hydraulics': [
+                    {'datetime':
+                        {'value': '2019-05-03T13:27:09.117623+00:00'}},
+                    {'datetime':
+                        {'value': '2019-05-03T15:27:09.117623+00:00'}}]}],
+                'publicid': ('smi:ch.ethz.sed/bh/'
+                             '11111111-e4a0-4692-bf29-33b5591eb798')}
+
+        s0 = HydraulicSample(
+            datetime_value=datetime.datetime(2019, 5, 3, 13, 27, 9, 117623))
+        s1 = HydraulicSample(
+            datetime_value=datetime.datetime(2019, 5, 3, 15, 27, 9, 117623))
+
+        hyd = Hydraulics(samples=[s0, s1])
+
+        sec = WellSection(
+            publicid=('smi:ch.ethz.sed/bh/section/'
+                      '11111111-8d89-4f13-95e7-526ade73cc8b'),
+            toplongitude_value=1172416.0953340977,
+            toplatitude_value=1159935.6543754074,
+            topdepth_value=217669.68585873488,
+            bottomlongitude_value=1172601.1309198323,
+            bottomlatitude_value=1160117.4947335268,
+            bottomdepth_value=216703.92402672302,
+            holediameter_value=0.3,
+            topclosed=False,
+            bottomclosed=False,
+            hydraulics=hyd)
+
+        bh = InjectionWell(
+            publicid='smi:ch.ethz.sed/bh/11111111-e4a0-4692-bf29-33b5591eb798',
+            sections=[sec])
+
+        proj = '+x_0=0 +y_0=0 +z_0=0'
+        serializer = HYDWSBoreholeHydraulicsSerializer(
+            proj=proj, transform_callback=pymap3d_transform_ned2geodetic)
+
+        self.assertEqual(serializer.dump(bh), reference_result)
+
+    def test_with_injectionplan(self):
+        # XXX(damb): Depending on the coordinate transformation used the
+        # results might vary.
+        reference_result = {
+            'sections': [{
+                'toplongitude': {'value': 10.663207130000002},
+                'toplatitude': {'value': 10.66320713},
+                'topdepth': {'value': 0.0},
+                'bottomlatitude': {'value': 10.66320713},
+                'bottomlongitude': {'value': 10.66320713},
+                'bottomdepth': {'value': 1000.0000000004027},
+                'holediameter': {'value': 0.3},
+                'topclosed': False,
+                'bottomclosed': False,
+                'publicid': ('smi:ch.ethz.sed/bh/section/'
+                             '11111111-8d89-4f13-95e7-526ade73cc8b'),
+                'hydraulics': [
+                    {'datetime':
+                        {'value': '2019-05-03T13:27:09.117623+00:00'}},
+                    {'datetime':
+                        {'value': '2019-05-03T15:27:09.117623+00:00'}}]}],
+                'publicid': ('smi:ch.ethz.sed/bh/'
+                             '11111111-e4a0-4692-bf29-33b5591eb798')}
+
+        s0 = HydraulicSample(
+            datetime_value=datetime.datetime(2019, 5, 3, 13, 27, 9, 117623))
+        s1 = HydraulicSample(
+            datetime_value=datetime.datetime(2019, 5, 3, 15, 27, 9, 117623))
+
+        plan = InjectionPlan(samples=[s0, s1])
+
+        sec = WellSection(
+            publicid=('smi:ch.ethz.sed/bh/section/'
+                      '11111111-8d89-4f13-95e7-526ade73cc8b'),
+            toplongitude_value=1172416.0953340977,
+            toplatitude_value=1159935.6543754074,
+            topdepth_value=217669.68585873488,
+            bottomlongitude_value=1172601.1309198323,
+            bottomlatitude_value=1160117.4947335268,
+            bottomdepth_value=216703.92402672302,
+            holediameter_value=0.3,
+            topclosed=False,
+            bottomclosed=False,
+            injectionplan=plan)
+
+        bh = InjectionWell(
+            publicid='smi:ch.ethz.sed/bh/11111111-e4a0-4692-bf29-33b5591eb798',
+            sections=[sec])
+
+        proj = '+x_0=0 +y_0=0 +z_0=0'
+        serializer = HYDWSBoreholeHydraulicsSerializer(
+            proj=proj, plan=True,
+            transform_callback=pymap3d_transform_ned2geodetic)
+
+        self.assertEqual(serializer.dump(bh), reference_result)
+
+
 def suite():
-    return unittest.makeSuite(HYDWSBoreholeHydraulicsDeserializer, 'test')
+    suite = unittest.TestSuite()
+    suite.addTest(
+        unittest.makeSuite(
+            HYDWSBoreholeHydraulicsDeserializerTestCase, 'test'))
+    suite.addTest(
+        unittest.makeSuite(HYDWSBoreholeHydraulicsSerializerTestCase, 'test'))
+    return suite
 
 
 if __name__ == '__main__':
