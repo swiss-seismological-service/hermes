784935a506bcac43f459cc407609d90b058c34da
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-06-13 10:43:06 +0000

    [TST]: Test injection plan import

--- RAMSIS/io/tests/test_hydraulics.py
+++ RAMSIS/io/tests/test_hydraulics.py
@@ -132,6 +132,43 @@ class HYDWSBoreholeHydraulicsDeserializerTestCase(unittest.TestCase):
         self.assertEqual(s1.datetime_value,
                          datetime.datetime(2019, 5, 3, 15, 27, 9, 117623))
 
+    def test_import_plan(self):
+        proj = '+x_0=0 +y_0=0 +z_0=0'
+        deserializer = HYDWSBoreholeHydraulicsDeserializer(
+            proj=proj, transform_callback=pymap3d_transform_geodetic2ned,
+            plan=True)
+
+        with open(os.path.join(self.PATH_RESOURCES, 'hyd.json'), 'rb') as ifs:
+            bh = deserializer.load(ifs)
+
+        self.assertEqual(len(bh.sections), 1)
+        self.assertEqual(
+            bh.publicid,
+            'smi:ch.ethz.sed/bh/11111111-e4a0-4692-bf29-33b5591eb798')
+
+        bh_section = bh.sections[0]
+        # borehole section coordinates
+        self.assertEqual(bh_section.toplongitude_value, 1172416.0953340977)
+        self.assertEqual(bh_section.toplatitude_value, 1159935.6543754074)
+        self.assertEqual(bh_section.topdepth_value, 217669.68585873488)
+        self.assertEqual(bh_section.bottomlongitude_value, 1172601.1309198323)
+        self.assertEqual(bh_section.bottomlatitude_value, 1160117.4947335268)
+        self.assertEqual(bh_section.bottomdepth_value, 216703.92402672302)
+        # additional borehole section attributes
+        self.assertEqual(len(bh_section.injectionplan.samples), 2)
+        self.assertEqual(
+            bh_section.publicid,
+            'smi:ch.ethz.sed/bh/section/11111111-8d89-4f13-95e7-526ade73cc8b')
+        self.assertEqual(bh_section.holediameter_value, 0.3)
+
+        # briefly validate samples
+        s0 = bh_section.injectionplan.samples[0]
+        self.assertEqual(s0.datetime_value,
+                         datetime.datetime(2019, 5, 3, 13, 27, 9, 117623))
+        s1 = bh_section.injectionplan.samples[1]
+        self.assertEqual(s1.datetime_value,
+                         datetime.datetime(2019, 5, 3, 15, 27, 9, 117623))
+
     def test_no_proj(self):
         deserializer = HYDWSBoreholeHydraulicsDeserializer(proj=None)
 
