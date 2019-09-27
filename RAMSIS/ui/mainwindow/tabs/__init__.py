ce24da940060f9d82d4d578133b0792bca006b00
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2019-09-27 12:08:15 +0000

    [STY] Remove header

--- RAMSIS/ui/mainwindow/tabs/__init__.py
+++ RAMSIS/ui/mainwindow/tabs/__init__.py
@@ -1,13 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Short Description
-
-Long Description
-    
-Copyright (C) 2015, SED (ETH Zurich)
-
-"""
-
 from .hazardtab import HazardTabPresenter
 from .modeltab import ModelTabPresenter
 #from .risktab import RiskTabPresenter
