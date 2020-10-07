2d19256a7e89deafc8d6bc7001f8d40eb87dd025
sarsonl <laura.sarson@sed.ethz.ch>
2020-10-07 10:36:39 +0200

    [BUG] rebase did not work properly

--- RAMSIS/ui/mainwindow/viewmodels/forecasttreemodel.py
+++ RAMSIS/ui/mainwindow/viewmodels/forecasttreemodel.py
@@ -72,7 +72,6 @@ class ForecastNode(Node):
         default = super().data(column, role)
 
         if role == Qt.DisplayRole and column == 0:
-<<<<<<< HEAD
             return self.item.id
         elif role == Qt.DisplayRole and column == 1:
             starttime = self.item.starttime
@@ -80,9 +79,6 @@ class ForecastNode(Node):
         elif role == Qt.DisplayRole and column == 2:
             forecast_status = self.item.status.state
             return forecast_status
-=======
-            return self.item.starttime.strftime('%d-%m-%Y %H:%M')
->>>>>>> [BUG] Fix dates consistently all to UTC (graph still showing error)
         return default
 
     def update_children(self, project):
