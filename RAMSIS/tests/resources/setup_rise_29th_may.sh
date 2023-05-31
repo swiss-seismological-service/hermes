beb75652b33222d7a59a44d7219b35cfdd5e1156
sarsonl <laura.sarson@sed.ethz.ch>
2023-05-31 16:57:28 +0200

    [TESTING] adding config for a test for rise 29th may

---
+++ RAMSIS/tests/resources/setup_rise_29th_may.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+(cd /home/sarsonl/repos/rt-ramsis/RAMSIS &&
+python tests/recreate_test_db.py &&
+ramsis model load --model-config tests/resources/model_etas_29th_may.json &&
+ramsis model load --model-config tests/resources/model_etas_altered_29th_may.json &&
+ramsis project create --config tests/resources/project_etas.json &&
+ramsis forecastseries create --config tests/resources/forecast_series_etas_29th_may.json &&
+ramsis forecastseries schedule 1 --overdue-interval 5000
+)
