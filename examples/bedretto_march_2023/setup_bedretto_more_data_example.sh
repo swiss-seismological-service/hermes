49e9e855b79d8e847321ac23f80dc30be020f096
Nicolas Schmid <nicolas@breiten.ch>
2024-06-26 15:55:28 +0200

    refactor: remove old code, keep locally in _old folder to stay consistent

--- examples/bedretto_march_2023/setup_bedretto_more_data_example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-(cd /home/ramsis/repo/rt-ramsis/examples/bedretto_march_2023 &&
-#ramsis model add-seismicity --model-config model_bedretto_22062022.json --hazardsourcemodeltemplate-path model_haz.json &&
-#ramsis model add-seismicity --model-config model_seis.json --hazardsourcemodeltemplate-path model_haz.json &&
-#ramsis model add-hazard --model-config model_haz.json --gsimlogictree-path gmpe_logic_tree.xml &&
-ramsis project create --config project_bedretto_14032023_no_ws.json &&
-ramsis forecast create --project-id 2 --config forecast_bedretto_14032023.json --inj-plan-directory /home/ramsis/repo/rt-ramsis/examples/bedretto_march_2023 --hyd-data well_data2023-03-14T11-00_2023-03-14T20-50.json --catalog-data catalog_data2023-03-14T11-00_2023-03-14T20-50.xml)
