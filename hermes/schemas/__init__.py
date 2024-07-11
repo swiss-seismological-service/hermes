65b10116355f8725c65b556224ac00a6afd64804
Nicolas Schmid <nicolas@breiten.ch>
2024-07-11 16:25:08 +0200

    refactor: rename model module to modelconfig

--- hermes/schemas/__init__.py
+++ hermes/schemas/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from hermes.schemas.base import EInput, EResultType, EStatus, Model
 from hermes.schemas.forecastseries import ForecastSeries
-from hermes.schemas.model import ModelConfig
+from hermes.schemas.modelconfig import ModelConfig
 from hermes.schemas.project import Project
 from hermes.schemas.tag import Tag
