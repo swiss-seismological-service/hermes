ba09a82fc563f2cc4b3e78e7a38445b92deaf5c1
Nicolas Schmid <nicolas@breiten.ch>
2024-07-17 12:28:28 +0200

    feat: improve get or create for tags with race condition check and refactor get_by_name

--- hermes/repositories/tag.py
+++ hermes/repositories/tag.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from hermes.datamodel import TagTable
@@ -10,17 +11,34 @@ class TagRepository(repository_factory(
         Tag, TagTable)):
 
     @classmethod
+    def _get_by_name(cls, session: Session, name: str) -> TagTable:
+        q = select(TagTable).where(TagTable.name == name)
+        result = session.execute(q).unique().scalar_one_or_none()
+        return result
+
+    @classmethod
+    def get_by_name(cls, session: Session, name: str) -> Tag:
+        result = cls._get_by_name(session, name)
+        return cls.model.model_validate(result) if result else None
+
+    @classmethod
     def _get_or_create(cls, session: Session, name: str) -> TagTable:
         q = select(TagTable).where(TagTable.name == name)
         result = session.execute(q).unique().scalar_one_or_none()
         if not result:
-            result = TagTable(name=name)
-            session.add(result)
-            session.commit()
-            session.refresh(result)
+            try:
+                result = TagTable(name=name)
+                session.add(result)
+                session.commit()
+                session.refresh(result)
+            except IntegrityError as e:
+                session.rollback()
+                result = cls._get_by_name(session, name)
+                if not result:
+                    raise e
         return result
 
     @classmethod
     def get_or_create(cls, session: Session, name: str) -> Tag:
         result = cls._get_or_create(session, name)
-        return cls.model.model_validate(result)
+        return cls.model.model_validate(result) if result else None
