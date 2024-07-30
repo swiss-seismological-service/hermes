fa5ce40bf7cc56b1f5234cd7571b93e2b8656b50
Nicolas Schmid <nicolas@breiten.ch>
2024-07-30 17:57:43 +0200

    fix: add session test to repo package for use throughout project

--- hermes/repositories/types.py
+++ hermes/repositories/types.py
@@ -1,7 +1,8 @@
-
 from geoalchemy2 import WKBElement
 from geoalchemy2.shape import to_shape
+from sqlalchemy.orm import Session
 
 # Type and conversion function for the DB model's polygon field
 PolygonType = WKBElement
 polygon_converter = to_shape
+SessionType = Session
