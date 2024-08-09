95edc44d8b1f4ffed4604e6c5b03379a7530d7bc
Nicolas Schmid <nicolas@breiten.ch>
2024-08-09 14:08:31 +0200

    test: update tests for forecast runner

--- hermes/flows/tests/test_create_forecast.py
+++ hermes/flows/tests/test_create_forecast.py
@@ -1,27 +1,31 @@
 from datetime import datetime
 from unittest.mock import MagicMock, patch
 
-from hermes.flows.create_forecast import ForecastExecutor
+from hermes.flows.forecast_runner import factory
 from hermes.repositories.types import SessionType
 from hermes.schemas import SeismicityObservation
 
 
 # too many mocks, however this function binds together the whole flow
 # of the forecast logic.
-@patch('hermes.flows.create_forecast.get_catalog')
+@patch('hermes.flows.forecast_runner.Session',)
+@patch('hermes.flows.forecast_runner.get_catalog')
 @patch('hermes.repositories.project.'
        'ForecastSeriesRepository.get_model_configs')
 @patch('hermes.repositories.data.SeismicityObservationRepository.create')
 @patch('hermes.repositories.project.ForecastRepository.create')
 @patch('hermes.repositories.project.ProjectRepository.get_by_id')
+@patch('hermes.repositories.project.ForecastSeriesRepository.get_by_id')
 class TestModels:
     def test_import(self,
-                    mock_p_c, mock_f_c, mock_obs_c, mock_conf_g, mock_cat_c,
+                    mock_fs_g, mock_p_c, mock_f_c, mock_obs_c, mock_conf_g,
+                    mock_cat_c, mock_ssn,
                     project, forecastseries, forecast, model_config):
 
         # mock all repository calls
-        mock_ssn = MagicMock(spec=SessionType)
+        mock_ssn.return_value = MagicMock(spec=SessionType)
         mock_cat_c().to_quakeml.return_value = 'data'
+        mock_fs_g.return_value = forecastseries
         mock_p_c.return_value = project
         mock_f_c.return_value = forecast
         mock_obs_c.return_value = SeismicityObservation(data='data')
@@ -29,9 +33,8 @@ class TestModels:
 
         starttime = datetime(2021, 1, 2, 0, 30, 0)
 
-        executor = ForecastExecutor(mock_ssn,
-                                    forecastseries,
-                                    starttime)
+        executor = factory(forecastseries.oid,
+                           starttime)
 
         assert executor.model_run_infos[0].config == model_config
         assert len(executor.model_run_infos) == 1
