183874a182d0031b85409bb4bb31f9a5b62552f8
Nicolas Schmid <nicolas@breiten.ch>
2024-08-12 16:11:51 +0200

    fix: ModelRunInfo needs to be serializable and small

--- hermes/schemas/model_schemas.py
+++ hermes/schemas/model_schemas.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from typing import Any
+from uuid import UUID
 
-from pydantic import ConfigDict
+from pydantic import ConfigDict, field_validator
 from shapely import Polygon
 
 from hermes.schemas.base import Model
@@ -24,5 +26,23 @@ class ModelInput(Model):
 
 
 class ModelRunInfo(Model):
+    forecast_oid: UUID | None = None
+    forecast_start: datetime | None = None
+    forecast_end: datetime | None = None
+
+    injection_observation_oid: UUID | None = None
+    injection_plan_oid: UUID | None = None
+    seismicity_observation_oid: UUID | None = None
+
+    bounding_polygon: str | None = None
+    depth_min: float | None = None
+    depth_max: float | None = None
+
     config: ModelConfig
-    input: ModelInput
+
+    @field_validator('bounding_polygon', mode='before')
+    @classmethod
+    def validate_bounding_polygon(cls, value: Any):
+        if isinstance(value, Polygon):
+            return value.wkt
+        return value
