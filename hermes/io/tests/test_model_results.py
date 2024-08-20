3b6fe4c02570e99d4777d46ea789533298251339
Nicolas Schmid <nicolas@breiten.ch>
2024-08-20 15:55:49 +0200

    tests: add test for save forecast catalog to repositories

--- hermes/io/tests/test_model_results.py
+++ hermes/io/tests/test_model_results.py
@@ -1,5 +1,5 @@
 import os
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pandas as pd
 from seismostats import ForecastCatalog
@@ -10,7 +10,19 @@ from hermes.io.model_results import save_forecast_catalog_to_repositories
 MODULE_LOCATION = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_save_forecast_catalog_to_repositories():
+@patch('hermes.io.model_results.TimeStepRepository.get_or_create',
+       autospec=True)
+@patch('hermes.io.model_results.GridCellRepository.get_or_create',
+       autospec=True)
+@patch('hermes.io.model_results.ModelResultRepository.batch_create',
+       autospec=True)
+@patch('hermes.io.model_results.SeismicEventRepository.'
+       'create_from_forecast_catalog',
+       autospec=True)
+def test_save_forecast_catalog_to_repositories(mock_seismic_event_repo,
+                                               mock_model_result_repo,
+                                               mock_grid_cell_repo,
+                                               mock_time):
     catalog_path = os.path.join(
         MODULE_LOCATION, '../../repositories/tests/data/catalog.parquet.gzip')
 
@@ -20,4 +32,4 @@ def test_save_forecast_catalog_to_repositories():
     catalog.bounding_polygon = from_wkt(
         'POLYGON ((45.7 5.85, 47.9 5.85, 47.9 10.6, 45.7 10.6, 45.7 5.85))')
 
-    # save_forecast_catalog_to_repositories(MagicMock(), catalog)
+    save_forecast_catalog_to_repositories(MagicMock(), None, None, catalog)
