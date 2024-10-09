aae3b347bbcf876c7675944475bdbff16b06c8a9
Nicolas Schmid <nicolas@breiten.ch>
2024-10-09 16:13:12 +0200

    feat: start on scheduler, constructing the forecast times in the past

--- hermes/flows/forecastseries_scheduler.py
+++ hermes/flows/forecastseries_scheduler.py
@@ -1,6 +1,102 @@
+# import asyncio
+from datetime import datetime, timedelta
+
+from hermes.schemas import ForecastSeries
+
+# from prefect.client.orchestration import get_client
+# from prefect.client.schemas.schedules import RRuleSchedule
+
+
 class ForecastSeriesScheduler:
-    def __init__(self):
-        pass
+    def __init__(self, forecastseries: ForecastSeries):
+        assert forecastseries.forecast_starttime is not None
+        assert forecastseries.forecast_interval is not None
+        assert forecastseries.forecast_endtime or \
+            forecastseries.forecast_duration
+
+        self.forecastseries = forecastseries
+        self.start = forecastseries.forecast_starttime
+        self.end = forecastseries.forecast_endtime
+        self.interval = forecastseries.forecast_interval
+        self.duration = forecastseries.forecast_duration
+        self.now = datetime.now()
+
+        self.past_forecasts = []
+        self.set_past_forecasts()
+
+    def set_past_forecasts(self):
+        # If the start time is in the future, we don't have any past forecasts
+        if self.start > self.now:
+            return None
+
+        # the overall end for past forecasts
+        if self.end is not None and self.end < self.now:
+            past_end = self.end
+        else:
+            past_end = self.now
+
+        # calculate the intervals for past forecasts
+        total_seconds = int((past_end - self.start).total_seconds())
+        intervals = range(0, total_seconds + 1, self.interval)
+
+        for i in intervals:
+            interval_start = self.start + timedelta(seconds=i)
+
+            # if the forecast has a duration, calculate the end times
+            if self.duration:
+                interval_end = interval_start + \
+                    timedelta(seconds=self.duration)
+            # if no duration is specified, use the overall end time
+            else:
+                interval_end = self.end
+
+            if not interval_end == interval_start:
+                self.past_forecasts.append((interval_start, interval_end))
+
+        next_start = self.past_forecasts[-1][0] + \
+            timedelta(seconds=self.interval)
+
+        if self.end is None or next_start < self.end:
+            self.start = next_start
 
     def run(self):
         pass
+
+
+# input to forecastflowrunner:
+# forecastseries: UUID,
+# starttime: datetime | None = None,
+# endtime: datetime | None = None
+
+# 1. get start and endtime for each forecast in the past
+# 2. get the next starttime in the future
+# 3. schedule ForecastFlowRunner with the next starttime
+#    and according to the parameters from the forecastseries
+
+
+# async def add_schedule_to_deployment():
+#     async with get_client() as client:
+#         deployment = await client.read_deployment(
+#             "your-flow-name/your-deployment-name")
+
+#         # Calculate start and end times
+#         start_time = datetime.now().replace(hour=16, minute=0, second=0,
+#                                             microsecond=0) + timedelta(days=1)
+#         end_time = start_time + \
+#             timedelta(days=(4 - start_time.weekday() + 7) % 7)  # Next Friday
+#         end_time = end_time.replace(hour=17, minute=0, second=0, microsecond=0)
+
+#         # Create RRule schedule
+#         rrule = f"FREQ=MINUTELY;INTERVAL=30;DTSTART={start_time.isoformat()}" \
+#                 f";UNTIL={end_time.isoformat()}"
+#         new_schedule = RRuleSchedule(rrule=rrule)
+
+#         # Add the new schedule to the deployment
+#         await client.create_deployment_schedule(
+#             deployment_id=deployment.id,
+#             schedule=new_schedule
+#         )
+
+# Run the async function
+
+# asyncio.run(add_schedule_to_deployment())
---
+++ hermes/flows/tests/test_forecastseries_scheduler.py
@@ -0,0 +1,99 @@
+from datetime import datetime, timedelta
+
+import pytest
+
+from hermes.flows.forecastseries_scheduler import ForecastSeriesScheduler
+from hermes.schemas import ForecastSeries
+
+
+class TestForecastSeriesScheduler:
+
+    def test_schedule_assertions(self):
+        """
+        Test that the assertions in the ForecastSeriesScheduler
+        constructor work.
+        """
+        forecastseries_wrong = ForecastSeries(
+            forecast_starttime=None,
+            forecast_endtime=datetime(2021, 1, 2, 12, 0, 0),
+            forecast_duration=1800,
+            forecast_interval=1800
+        )
+
+        with pytest.raises(AssertionError):
+            ForecastSeriesScheduler(forecastseries_wrong)
+
+        forecastseries_wrong.forecast_starttime = datetime(2021, 1, 2, 0, 0, 0)
+        forecastseries_wrong.forecast_interval = None
+
+        with pytest.raises(AssertionError):
+            ForecastSeriesScheduler(forecastseries_wrong)
+
+    def test_past(self):
+        """
+        Test the past forecast times generation.
+        """
+        forecastseries = ForecastSeries(
+            forecast_starttime=datetime(2021, 1, 2, 0, 0, 0),
+            forecast_endtime=datetime(2021, 1, 2, 12, 0, 0),
+            forecast_duration=1800,
+            forecast_interval=1800
+        )
+
+        scheduler = ForecastSeriesScheduler(forecastseries)
+        assert len(scheduler.past_forecasts) == 25
+        assert scheduler.past_forecasts[0] == (
+            datetime(2021, 1, 2, 0, 0, 0),
+            datetime(2021, 1, 2, 0, 30, 0)
+        )
+
+        assert scheduler.past_forecasts[-1] == (
+            datetime(2021, 1, 2, 12, 0, 0),
+            datetime(2021, 1, 2, 12, 30, 0)
+        )
+
+    def test_past_future_end(self):
+
+        # Test the past forecast times generation with a future end time.
+        now = datetime.now()
+        before = now - timedelta(hours=2)
+        after = now + timedelta(hours=2)
+        next = now + timedelta(minutes=30)
+
+        forecastseries = ForecastSeries(
+            forecast_starttime=before,
+            forecast_endtime=after,
+            forecast_duration=1800,
+            forecast_interval=1800
+        )
+
+        scheduler = ForecastSeriesScheduler(forecastseries)
+        assert len(scheduler.past_forecasts) == 5
+        assert abs(scheduler.start - next) < timedelta(seconds=1)
+
+        # Test with no end time
+        forecastseries.forecast_endtime = None
+        scheduler = ForecastSeriesScheduler(forecastseries)
+        assert len(scheduler.past_forecasts) == 5
+        assert abs(scheduler.start - next) < timedelta(seconds=1)
+
+    def test_no_duration(self):
+        # Test the past forecast times generation with no duration
+        forecastseries = ForecastSeries(
+            forecast_starttime=datetime(2021, 1, 2, 0, 0, 0),
+            forecast_endtime=datetime(2021, 1, 2, 12, 0, 0),
+            forecast_duration=None,
+            forecast_interval=1800
+        )
+
+        scheduler = ForecastSeriesScheduler(forecastseries)
+        assert len(scheduler.past_forecasts) == 24
+        assert scheduler.past_forecasts[0] == (
+            datetime(2021, 1, 2, 0, 0, 0),
+            datetime(2021, 1, 2, 12, 0, 0)
+        )
+
+        assert scheduler.past_forecasts[-1] == (
+            datetime(2021, 1, 2, 11, 30, 0),
+            datetime(2021, 1, 2, 12, 0, 0)
+        )
