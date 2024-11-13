06c475971d7aafbfcf890f480d959662cd4c1c90
Nicolas Schmid <nicolas@breiten.ch>
2024-11-13 17:52:58 +0100

    chores: move manual test run files

---
+++ scripts/natural/run.sh
@@ -0,0 +1,3 @@
+source env/bin/activate
+hermes forecast run test_forecastseries --start 2024-10-28T00:00:00 --end 2024-10-30T00:00:00
+deactivate
--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-source env/bin/activate
-hermes forecast run test_forecastseries --start 2024-10-28T00:00:00 --end 2024-10-30T00:00:00
-deactivate
