6781be6385e6b8f20040642966b1e29a927bd597
Nicolas Schmid <nicolas@breiten.ch>
2024-11-27 17:49:19 +0100

    chores: add example scripts for catalog forecasts

--- scripts/induced/run.sh
+++ scripts/induced/run.sh
@@ -1,4 +1,6 @@
 source env/bin/activate
+hermes drop-tables
+hermes create-tables
 hermes project create project_induced --config scripts/induced/project.json
 hermes forecastseries create fs_induced --config scripts/induced/forecastseries.json --project project_induced
 hermes injectionplan create default --forecastseries fs_induced --file scripts/induced/injectionplan.json
--- scripts/natural/run.sh
+++ scripts/natural/run.sh
@@ -1,3 +1,8 @@
 source env/bin/activate
-hermes forecast run test_forecastseries --start 2024-10-28T00:00:00 --end 2024-10-30T00:00:00
+hermes drop-tables
+hermes create-tables
+hermes project create project_natural --config scripts/natural/project.json
+hermes forecastseries create fs_natural --config scripts/natural/forecastseries.json --project project_natural
+hermes model create etas --config scripts/natural/model_config.json
+hermes forecast run fs_natural --start 2024-12-01T00:00:00 --end 2024-12-31T00:00:00
 deactivate
